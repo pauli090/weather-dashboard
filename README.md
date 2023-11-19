61378dd0a8326990d4073ecefb15627bcf7c128a
pauli090 <paulinkaa090@gmail.com>
2023-11-19 20:03:07 +0000

    updated css file and js file, added today's forecast js code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # weather-dashboard
 An app that helps a user to see the weather outlook for multiple cities.
+
+
+https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+https://stackoverflow.com/questions/44476751/how-to-append-image-and-header-using-jquery
+https://stackoverflow.com/questions/28239929/css-display-header-and-button-in-same-line
+https://getbootstrap.com/docs/5.0/getting-started/introduction/
+https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+https://day.js.org/docs/en/display/format
+https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function checkWheather(city) {
     // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=4744a375f85ea94ec6e376bf29c19986";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=4744a375f85ea94ec6e376bf29c19986";
     fetch(queryURL)
       .then(function (response) {
         return response.json();
@@ -11,18 +11,30 @@ function checkWheather(city) {
         // Printing the entire object to console
         console.log(queryURL);
         console.log(data);
-  
-        // Constructing HTML containing the artist information
-        // var artistName = $("<h1>").text(data.name);
-        // var artistURL = $("<a>").attr("href", data.url).append(artistName);
-        // var artistImage = $("<img>").attr("src", data.thumb_url);
-        // var trackerCount = $("<h2>").text(data.tracker_count + " fans tracking this artist");
-        // var upcomingEvents = $("<h2>").text(data.upcoming_event_count + " upcoming events");
-        // var goToArtist = $("<a>").attr("href", data.url).text("See Tour Dates");
-  
-        // // Empty the contents of the artist-div, append the new artist content
-        // $("#artist-div").empty();
-        // $("#artist-div").append(artistURL, artistImage, trackerCount, upcomingEvents, goToArtist);
+
+        var cityName = data.city.name;
+        var day = dayjs().format("DD/MM/YYYY");
+        var displacyCity = $("<h2 id=todayCity>").text(cityName + " (" + day + ")" + " ");
+        var weatherIcon = data.list[0].weather[0].icon;
+        console.log(weatherIcon);
+        var iconURL = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+        console.log(iconURL);
+        var weatherIconImage = $("<img id=icon>").attr("src", iconURL);
+        
+      //  $("#today").append(weatherIconImage);
+
+        console.log(weatherIconImage);
+        var temp = data.list[0].main.temp;
+        var tempDisplay = $("<p>").text("Temp: " + temp + " °C");
+       // $("#today").append(tempDisplay);
+        var wind = data.list[0].wind.speed;
+        var windDisplay = $("<p>").text("Wind: " + wind + " KPH");
+      //  $("#today").append(windDisplay);
+        var humidity = data.list[0].main.humidity;
+        var humidityDisplay = $("<p>").text("Humidity: " + humidity + " %");
+      //  $("#today").append(humidityDisplay);
+        $("#today").empty();
+        $("#today").append(displacyCity, weatherIconImage, tempDisplay, windDisplay, humidityDisplay);
       })
     }
 $("#search-button").on("click", function(event) {
