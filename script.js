c1b0bbe1a2f4af52209a2de77c31927ffd33ea39
pauli090 <paulinkaa090@gmail.com>
2023-11-23 18:22:47 +0000

    updated js file

--- script.js
+++ script.js
@@ -11,32 +11,72 @@ function checkWheather(city) {
         // Printing the entire object to console
         console.log(queryURL);
         console.log(data);
+        var day = dayjs().format("DD/MM/YYYY");
+        var dayStart = dayjs().startOf('D');
+        var dayEnd = dayjs().endOf('D');
+        
+        // for (var i = 0; i < data.list.length; i++) {
+        //   var today = data.list[i].dt_txt;
+          
+        //   if (dayjs().isSame(today, "day")) {
+        //     console.log(data.list[0].dt_txt);
+        //   } else if (dayjs().add(1, 'day').isSame(today, "day")) {
+        //     console.log(data.list[1].dt_txt);
+        //   } else if (dayjs().add(2, 'day').isSame(today, "day")) {
+        //     console.log(data.list[2].dt_txt);
+        //   } else if (dayjs().add(3, 'day').isSame(today, "day")) {
+        //     console.log(data.list[3].dt_txt);
+        //   } else  {
+        //       console.log(data.list[4].dt_txt);
+        //   }
+        // }
 
         var cityName = data.city.name;
-        var day = dayjs().format("DD/MM/YYYY");
-        var displacyCity = $("<h2 id=todayCity>").text(cityName + " (" + day + ")" + " ");
-        var weatherIcon = data.list[0].weather[0].icon;
-        console.log(weatherIcon);
-        var iconURL = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-        console.log(iconURL);
-        var weatherIconImage = $("<img id=icon>").attr("src", iconURL);
         
-      //  $("#today").append(weatherIconImage);
-
-        console.log(weatherIconImage);
+        var displacyCity = $("<h2 id=todayCity>").text(cityName + " (" + day + ")" + " ");
+        
+        var weatherIcon = data.list[0].weather[0].icon;
+        var iconURL = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+        var weatherIconImage = $("<img class=icon>").attr("src", iconURL);
+        
+     
         var temp = data.list[0].main.temp;
         var tempDisplay = $("<p>").text("Temp: " + temp + " °C");
-       // $("#today").append(tempDisplay);
+       
         var wind = data.list[0].wind.speed;
         var windDisplay = $("<p>").text("Wind: " + wind + " KPH");
-      //  $("#today").append(windDisplay);
+
         var humidity = data.list[0].main.humidity;
         var humidityDisplay = $("<p>").text("Humidity: " + humidity + " %");
-      //  $("#today").append(humidityDisplay);
+
         $("#today").empty();
         $("#today").append(displacyCity, weatherIconImage, tempDisplay, windDisplay, humidityDisplay);
+      
+        var fiveDay = $("<h3>").text("5-day forecast");
+        $("#forecast").append(fiveDay);
+        
+        var dayTwo = $("<h4>").text(dayjs().add(1, 'day').format("DD/MM/YYYY"));
+        var weatherIconTwo = data.list[1].weather[0].icon;
+        var iconURLTwo = "https://openweathermap.org/img/wn/" + weatherIconTwo + "@2x.png";
+        var weatherIconImageTwo = $("<img class=icon>").attr("src", iconURLTwo);
+        var tempTwo = data.list[1].main.temp;
+        var tempDisplayTwo = $("<p>").text("Temp: " + tempTwo + " °C");
+       
+        var windTwo = data.list[1].wind.speed;
+        var windDisplayTwo = $("<p>").text("Wind: " + windTwo + " KPH");
+
+        var humidityTwo = data.list[1].main.humidity;
+        var humidityDisplayTwo = $("<p>").text("Humidity: " + humidityTwo + " %");
+        var divOne = $("<div id=colOne>");
+        $("#forecast").append(divOne);
+        $("#colOne").append(dayTwo, weatherIconImageTwo, tempDisplayTwo, windDisplayTwo, humidityDisplayTwo);
+        
       })
     }
+
+
+
+
 $("#search-button").on("click", function(event) {
 
     event.preventDefault();
